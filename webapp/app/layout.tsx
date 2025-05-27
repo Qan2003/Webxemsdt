2683620e60d5aaefe07f0b8866c0d2e9928c21f0
QuocAn <an0981806943@gmail.com>
2025-05-27 09:16:16 +0700

    ss

--- webapp/app/layout.tsx
+++ webapp/app/layout.tsx
@@ -8,21 +8,17 @@ const playfair = Playfair_Display({ subsets: ["latin"], variable: "--font-serif"
 const nunito = Nunito_Sans({ subsets: ["latin"], variable: "--font-body", display: "swap" })
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
-  return (
-    <html lang="vi" className={`scroll-smooth ${inter.variable} ${playfair.variable} ${nunito.variable}`}>
-      <head>
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
-      </head>
-      <body className="font-sans antialiased">
-        <ThemeProvider attribute="class" defaultTheme="light" enableSystem>
-          {children}
-        </ThemeProvider>
-      </body>
-    </html>
-  )
+    return (
+        <html lang="vi" className="scroll-smooth" suppressHydrationWarning>
+            <head>
+                <link rel="preconnect" href="https://fonts.googleapis.com" />
+                <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
+            </head>
+            <body className={`font-sans antialiased ${inter.variable} ${playfair.variable} ${nunito.variable}`}>
+                <ThemeProvider attribute="class" defaultTheme="light" enableSystem>
+                    {children}
+                </ThemeProvider>
+            </body>
+        </html>
+    )
 }
-
-export const metadata = {
-      generator: 'v0.dev'
-    };
